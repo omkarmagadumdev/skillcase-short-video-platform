aac5a099844be5f51f89783ade782fa49b6495cb
Omkar Magadum <omkar.magadum.dev@gmail.com>
2026-06-19 16:54:49 +0530

    feat(frontend): establish frontend foundation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
-function App() {
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import NotFound from "./pages/NotFound";
+import { HOME, LOGIN, REGISTER, NOT_FOUND } from "./utils/routes";
+
+const App = () => {
   return (
-    <main>
-      <h1>Skillcase Short Video Platform</h1>
-      <p>Milestone 1 project scaffold is ready.</p>
-    </main>
+    <Routes>
+      <Route path={HOME} element={<Home />} />
+      <Route path={LOGIN} element={<Login />} />
+      <Route path={REGISTER} element={<Register />} />
+      <Route path={NOT_FOUND} element={<NotFound />} />
+    </Routes>
   );
-}
+};
 
 export default App;
